653cd0c625840c4e103c0177c25081374f6a4a1b
arifislamarif344 <arifislamarif344@gmail.com>
2022-05-14 18:20:16 +0600

    useState

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import React from 'react';
-import Section from './components/Section';
-import ThemeContext from './contexts/themeContext';
+import Todo from './components/Todo';
 
 class App extends React.Component {
-    state = {
-        theme: 'dark',
-    };
-
-    switchTheme = () => {
-        this.setState(({ theme }) => {
-            if (theme === 'dark') {
-                return {
-                    theme: 'light',
-                };
-            }
-            return {
-                theme: 'dark',
-            };
-        });
-    };
-
     render() {
-        const { theme } = this.state;
         return (
             <div className="app">
-                <ThemeContext.Provider value={{ theme, switchTheme: this.switchTheme }}>
-                    <Section />
-                </ThemeContext.Provider>
+                <Todo />
             </div>
         );
     }
---
+++ src/components/TodoClass.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class Todo extends React.Component {
+    state = {
+        todo: '',
+        warning: null,
+    };
+
+    handleInput = (e) => {
+        const inputValue = e.target.value;
+        const warning = inputValue.includes('.js') ? 'You need JS Skill' : null;
+
+        this.setState({
+            todo: inputValue,
+            warning,
+        });
+    };
+
+    render() {
+        const { todo, warning } = this.state;
+
+        return (
+            <div>
+                <p>{todo}</p>
+                <p>
+                    <textarea name="todo" value={todo} onChange={this.handleInput} />
+                </p>
+                <hr />
+                <h2>{warning || 'Good choice!'}</h2>
+            </div>
+        );
+    }
+}
+
+export default Todo;
