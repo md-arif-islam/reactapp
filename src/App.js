d845a3b9e660e56cd931a82da2d8aeb5e4ab88be
arifislamarif344 <arifislamarif344@gmail.com>
2022-05-15 16:30:14 +0600

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
-import { useCallback, useMemo, useState } from 'react';
-import Button from './components/Button';
-import ShowCount from './components/ShowCount';
-import Title from './components/Title';
+import GetPost from './components/GetPost';
 
-function App() {
-    const [count1, setCount1] = useState(0);
-    const [count2, setCount2] = useState(0);
-
-    const incrementByOne = useCallback(() => {
-        setCount1((prevCount) => prevCount + 1);
-    }, []);
-
-    const incrementByFive = useCallback(() => {
-        setCount2((prevCount) => prevCount + 5);
-    }, []);
-
-    const isEvenOrOdd = useMemo(() => {
-        let i = 0;
-        while (i < 1000000000) i += 1; // costly operation
-        return count1 % 2 === 0;
-    }, [count1]);
-
+export default function App() {
     return (
         <div className="app">
-            <Title />
-            <ShowCount count={count1} title="Counter 1" />
-            <span>{isEvenOrOdd ? 'Even' : 'Odd'}</span>
-            <Button handleClick={incrementByOne}>Increment by one</Button>
-            <hr />
-            <ShowCount count={count2} title="Counter 2" />
-            <Button handleClick={incrementByFive}>Increment by five</Button>
+            <div>
+                <GetPost />
+            </div>
         </div>
     );
 }
-
-export default App;
--- src/components/Title.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Title() {
-    console.log('rendering title...');
-
-    return <h2>useCallback Hook Tutorial</h2>;
-}
-
-export default React.memo(Title);
