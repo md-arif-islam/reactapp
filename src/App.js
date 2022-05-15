a65e9ad30aca6af4be75ffbf812874879f5f4612
arifislamarif344 <arifislamarif344@gmail.com>
2022-05-15 10:37:49 +0600

    useEffect()

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import Todo from './components/Todo';
+import { useState } from 'react';
+import MyComponent from './components/MyComponent';
 
-class App extends React.Component {
-    render() {
-        return (
-            <div className="app">
-                <Todo />
-            </div>
-        );
-    }
+export default function App() {
+    const [show, setShow] = useState(true);
+
+    return (
+        <div className="app">
+            <div>{show && <MyComponent />}</div>
+            <p>
+                <button type="button" onClick={() => setShow((prevShow) => !prevShow)}>
+                    {show ? 'Hide post' : 'Show post'}
+                </button>
+            </p>
+        </div>
+    );
 }
-
-export default App;
