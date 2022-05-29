a6a9a08baacaa6ea2240df9afc8282b619c336cf
arifislamarif344 <arifislamarif344@gmail.com>
2022-05-29 06:38:48 +0600

    Styled Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './assets/css/global.css';
-import Logo from './components/Logo';
+// import './assets/css/dark.css';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './components/styles/Global.styles';
+import Tutorial from './components/Tutorial';
 
 export default function App() {
+    const theme = {
+        main: 'white',
+    };
     return (
-        <div className="App">
-            <h1>Learn with Sumit</h1>
-            <Logo />
-        </div>
+        <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <Tutorial />
+        </ThemeProvider>
     );
 }
